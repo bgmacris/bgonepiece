1dd0d0631e5cbbf15ecb1055cedbda435a198799
bgmacris <bgmacris9999@gmail.com>
2024-03-21 11:34:56 +0100

    Nuevas funcionalidades JS, mover columna tareas, botones interactivos. Cambio estetica del kamban

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, create_task
+from .views import index, create_task, actualizar_tarea_estado
 
 urlpatterns = [
     path("roadmap/", index, name="roadmap"),
     path('roadmap/create_task/', create_task, name='create_task'),
+    path('actualizar_tarea_estado/', actualizar_tarea_estado, name='actualizar_tarea_estado'),
 ]
--- roadmap/views.py
+++ roadmap/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from .models import Task
 from .forms import TaskForm
 
 # Create your views here.
 def index(request):
     tasks = Task.objects.all()
-    return render(request, 'roadmap.html', {'tasks': tasks, 'task_form': TaskForm, 'is_admin': request.user.is_superuser})
+
+    task_todo = tasks.filter(status='todo')
+    task_in_progress = tasks.filter(status='in_progress')
+    task_done = tasks.filter(status='done')
+
+    context = {
+        'tasks': tasks,
+        'task_form': TaskForm,
+        'is_admin': request.user.is_superuser,
+        'task_todo': task_todo,
+        'task_in_progress': task_in_progress,
+        'task_done': task_done,
+    }
+
+    return render(request, 'roadmap.html', context)
 
 
 @login_required
@@ -19,3 +34,18 @@ def create_task(request):
     else:
         form = TaskForm()
     return redirect('roadmap')
+
+@login_required
+def actualizar_tarea_estado(request):
+    if request.method == 'POST':
+        id_tarea = request.POST.get('id')
+        columna_destino = request.POST.get('id_status')
+        try:
+            tarea = Task.objects.get(pk=id_tarea)
+            tarea.status = columna_destino
+            tarea.save()
+            return JsonResponse({'status': 'success'})
+        except:
+            return JsonResponse({'status': 'error'})
+    else:
+        return JsonResponse({'status': 'error'})
