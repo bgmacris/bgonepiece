6268cc60f00d01d548abaaaf8344496ee67e3c0b
bgmacris <bgmacris9999@gmail.com>
2024-03-21 14:11:43 +0100

    Solucionar responsive de la tabla kanban de tareas

--- static/js/roadmap.js
+++ static/js/roadmap.js
@@ -1,3 +1,31 @@
+// Función para ajustar la altura de las columnas del kamban
+window.onload = function () {
+    function ajustarAlturaColumnas() {
+        var columnas = document.querySelectorAll('.column');
+        columnas.forEach(function (columna) {
+            let alturaTotal = columna.querySelector('h2').offsetHeight + 11;
+            var tareas = columna.querySelectorAll('.task');
+            if (tareas.length === 0) {
+                alturaTotal += 60;
+            }
+            tareas.forEach(function (tarea) {
+                alturaTotal += tarea.offsetHeight + 10;
+            });
+            alturaTotal += 15
+            columna.style.height = alturaTotal + 'px';
+        });
+    }
+
+    // Llamar a la función de ajuste de altura cuando se carga la página
+    ajustarAlturaColumnas();
+
+    // Llamar a la función de ajuste de altura cuando se redimensiona la ventana
+    window.addEventListener('resize', function () {
+        ajustarAlturaColumnas();
+    });
+};
+
+
 // FUNCIONES para mover las tareas de columnas
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.dataset.id);
@@ -8,32 +36,32 @@ function allowDrop(ev) {
 }
 
 function drop(ev, columna_destino) {
-        var data = ev.dataTransfer.getData("text");
-        var tarea = document.getElementById(data);
+    var data = ev.dataTransfer.getData("text");
+    var tarea = document.getElementById(data);
 
-        actualizarEstadoTarea(data, columna_destino);
-    }
+    actualizarEstadoTarea(data, columna_destino);
+}
 
 function actualizarEstadoTarea(id_tarea, columna_destino) {
-        var csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-        var url = '/actualizar_tarea_estado/'; // URL de la vista que manejará la actualización de estado
-        var formData = new FormData();
-        formData.append('id', id_tarea);
-        formData.append('id_status', columna_destino);
+    var csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    var url = '/actualizar_tarea_estado/'; // URL de la vista que manejará la actualización de estado
+    var formData = new FormData();
+    formData.append('id', id_tarea);
+    formData.append('id_status', columna_destino);
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', url, true);
-        xhr.setRequestHeader('X-CSRFToken', csrfToken);
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                console.log('Estado de tarea actualizado correctamente');
-            } else {
-                console.error('Error al actualizar estado de tarea');
-            }
-        };
-        xhr.send(formData);
-        location.reload();
-    }
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('X-CSRFToken', csrfToken);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            console.log('Estado de tarea actualizado correctamente');
+        } else {
+            console.error('Error al actualizar estado de tarea');
+        }
+    };
+    xhr.send(formData);
+    location.reload();
+}
 
 
 // FUNCIONES para manejar popups tareas
@@ -53,22 +81,12 @@ function closePopup() {
     document.getElementById('popup').style.display = 'none';
 }
 
-window.onload = function () {
-        var columnas = document.querySelectorAll('.column');
-        columnas.forEach(function (columna) {
-            var tareas = columna.querySelectorAll('.task');
-            var alturaTotal = 100;
-            tareas.forEach(function (tarea) {
-                alturaTotal += tarea.offsetHeight;
-            });
-            columna.style.height = alturaTotal + 'px';
-        });
-    };
-
 
 // FUNCIONES para manejar creacion de tareas
 // Función para abrir el modal y establecer el estado de la tarea
 function abrirModal(estado) {
+    // Agregar evento de clic al botón de cerrar modal
+    document.getElementById("close-modal-btn").addEventListener("click", cerrarModal);
     // Establecer el estado de la tarea en el formulario
     console.log("AA", estado)
     document.getElementById("id_status").value = estado;
@@ -90,9 +108,6 @@ function cerrarModal() {
     var modal = document.getElementById("modal");
     modal.style.display = "none"; // Ocultar el modal
 }
-
-// Agregar evento de clic al botón de cerrar modal
-document.getElementById("close-modal-btn").addEventListener("click", cerrarModal);
 
 // Cerrar el modal si se hace clic fuera del modal
 window.addEventListener("click", function (event) {
